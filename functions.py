b89205fefb5e5739db0bab287f40b27533011c9f
Thir133nth <gurtnetua@gmail.com>
2023-09-03 21:14:58 +0300

    Bugfix 0.04

--- functions.py
+++ functions.py
@@ -14,15 +14,15 @@ def write_todos(todos_arg, filepath=FILEPATH):
         file.writelines(todos_arg)
 
 
-def get_done_todos(filepath=FILEPATH2):
-    with open(filepath, 'r') as file_local2:
+def get_done_todos(filepath2=FILEPATH2):
+    with open(filepath2, 'r') as file_local2:
         done_todos_local = file_local2.readlines()
     return done_todos_local
 
 
-def write_done_todos(done_todos_arg, filepath=FILEPATH2):
+def write_done_todos(done_todos_arg, filepath2=FILEPATH2):
     """ Writes todo items list in the text file"""
-    with open(filepath, 'w') as file2:
+    with open(filepath2, 'w') as file2:
         file2.writelines(done_todos_arg)
 
 
